a1662b9d88ea12feeebfea74c66d6518aa528627
Samuel <knightincode@gmail.com>
2024-06-24 14:53:36 +0000

    finished cahllenge

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,15 +1,19 @@
 // Learning C++ 
 // Challenge 01_03
-// Console Interaction, by Eduardo Corpeño 
+// Console Interaction, by Eduardo Corpeño
+//Samuel Knight Repository 
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    
+    string str; //hold user input
+    cout << "Enter your name: " << flush;
+    cin >> str; //captures input from keyboard
+    cout << "Nice to meet you " + str << "!" <<  endl; //displays string variable
 
-    std::cout << std::endl << std::endl;
+    cout << endl << endl;
     return (0);
 }
