a026dd60cffe980beedf9bb20fcf2eb521eb9137
Samuel <knightincode@gmail.com>
2024-06-24 16:27:16 +0000

    no-need challenge

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -1,10 +1,15 @@
 // Learning C++ 
 // Exercise 02_08
 // Arrays, by Eduardo Corpeño 
+//samuel knight
 
 #include <iostream>
 
 int main(){
+    int age[4];
+
+    age[0] = 25;
+    //this challenge owrks with arrays but at the simple level. no need to practice this
     
     std::cout << std::endl << std::endl;
     return (0);
