4af189167dfc94d08242a634cc44f5ba4d39af75
Samuel <knightincode@gmail.com>
2024-06-29 19:46:29 +0000

    finished challenge

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -4,8 +4,25 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
+
+enum class cow_purpose {dairy, meat, hide, pet};
+
+struct cow {
+    string name;
+    int age;
+    cow_purpose purpose;
+};
 
 int main(){
+
+    cow my_cow;
+    my_cow.age = 5;
+    my_cow.name = "Bestsy";
+    my_cow.purpose = cow_purpose::dairy;
+    cout << my_cow.name << " is a type-" <<(int)my_cow.purpose << " cow" << endl;
+    cout << my_cow.name << " is " << my_cow.age << " years old." << endl;
+
     
     std::cout << std::endl << std::endl;
     return (0);
