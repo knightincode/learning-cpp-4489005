62bf76366111e406d838a8b1ca716a367a4b9db9
Samuel <knightincode@gmail.com>
2024-06-30 16:34:15 +0000

    finish

--- src/Ch04/04_02b/CodeDemo.cpp
+++ src/Ch04/04_02b/CodeDemo.cpp
@@ -1,21 +1,41 @@
 // Learning C++ 
 // Exercise 04_02
 // Switch Statements, by Eduardo Corpeño 
+//samuel k
 
 #include <iostream>
+using namespace std;
 
 int main(){
-    float operand_1, operand_2, result;
+    float num1, num2, result;
     char operation;
 
-    std::cout << "Enter operand 1: " << std::flush;
-    std::cin >> operand_1;
-    std::cout << "Enter operand 2: " << std::flush;
-    std::cin >> operand_2;
+    std::cout << "Enter number 1: " << std::flush;
+    std::cin >> num1;
+    std::cout << "Enter number 2: " << std::flush;
+    std::cin >> num2;
     std::cout << "Choose operation [ + - * / ]: " << std::flush;
     std::cin >> operation;
 
     // switch goes here
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = num1 + num2;
+            break;
+
+    }
 
     std::cout << "The result is " << result << std::endl;
     
