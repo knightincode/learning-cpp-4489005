a2566ee92297dd16300ec334e76382a93c993632
Samuel <knightincode@gmail.com>
2024-06-30 15:56:27 +0000

    finished

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -1,11 +1,26 @@
 // Learning C++ 
 // Exercise 04_01
 // If Statements, by Eduardo Corpeño 
+//samuel knight
 
 #include <iostream>
+using namespace std;
 
 int main(){
+
+    int a = 1023;
+    bool flag = false;
+
+    if (a > 1000)
+        cout << "WARNING: a is over 1000. " << endl;
+
+    if  (a % 2 == 0)
+        cout << "a is even." << endl;
+    else {
+        cout << "a is odd." << endl;
+    }
     
-    std::cout << std::endl << std::endl;
+
+    cout << endl << endl;
     return (0);
 }
